c12e2bfcddda7b94a5b6ebcefce562af18d4a31f
saifrizvi7 <61203261+saifrizvi7@users.noreply.github.com>
2021-12-22 16:42:44 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest
+from calculation import calculation
+#Test cases to test Calulator methods
+#You always create  a child class derived from unittest.TestCase
+class test(unittest.TestCase):
+  #setUp method is overridden from the parent class TestCase
+  def setUp(self):
+    self.Calc = calculation()
+  #Each test method starts with the keyword test_
+  def test_add(self):
+    self.assertEqual(self.Calc.addition(4,7), 11)
+  def test_subtract(self):
+    self.assertEqual(self.Calc.subtraction(10,5), 5)
+  def test_multiply(self):
+    self.assertEqual(self.Calc.multiplication(3,7), 21)
+  def test_divide(self):
+    self.assertEqual(self.Calc.division(10,2), 5)
+  def test_modulus(self):
+    self.assertEqual(self.Calc.modulo(3,2), 1)
+  def test_exponential(self):
+    self.assertEqual(self.Calc.exponential(2,3), 8)
+  def test_floor(self):
+    self.assertEqual(self.Calc.floor_division(3,1), 3)
+# Executing the tests in the above test case class
+if __name__ == "__main__":
+  unittest.main()
